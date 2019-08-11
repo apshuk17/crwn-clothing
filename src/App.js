f4e578ba07baa5c3264a025ca1e47ae3f5422b95
Apoorva Shukla <apshuk17@gmail.com>
2019-08-11 10:40:46 +0530

    Added header component to the app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import HomePage from './pages/homepage/home.component';
-import ShopPage from './pages/shop/shop.component.jsx';
-import './App.scss';
+import HomePage from "./pages/homepage/home.component";
+import ShopPage from "./pages/shop/shop.component.jsx";
+import Header from "./components/header/header.component";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
-        <Route exact path={'/'} component={HomePage}></Route>
-        <Route path={'/shop'} component={ShopPage}></Route>
+        <Route exact path={"/"} component={HomePage} />
+        <Route path={"/shop"} component={ShopPage} />
       </Switch>
     </div>
   );
--- src/pages/homepage/home.component.jsx
+++ src/pages/homepage/home.component.jsx
@@ -3,10 +3,9 @@ import React from 'react';
 import Directory from '../../components/directory/directory.component'; 
 import './homepage.styles.scss';
 
-const Homepage = ({ history }) => {
+const Homepage = () => {
     return (
         <div className='homepage'>
-            <button onClick={() => history.push('/shop')}>Shop Page</button>
             <div className='directory-menu'>
                 <Directory/>
             </div>
